cfec15fb0d7a093dd72a1d2ee1b6dc932a78ecd4
JayJay2018 <hello@jrfranke.com>
2022-12-20 18:31:55 +0000

    Feature complete: Dictionary check of attempt

--- longest_word/game.py
+++ longest_word/game.py
@@ -2,6 +2,9 @@
 # pylint: disable=too-few-public-methods
 import random
 import string
+import requests
+
+BASE = 'https://wagon-dictionary.herokuapp.com/'
 
 class Game:
     def __init__(self) -> list:
@@ -11,6 +14,7 @@ class Game:
 
     def is_valid(self, word: str) -> bool:
         """Return True if and only if the word is valid, given the Game's grid"""
+        # if  word == '':
         if not word:
             return False
         letters = self.grid.copy() # Consume letters from the grid
@@ -19,4 +23,8 @@ class Game:
                 letters.remove(letter)
             else:
                 return False
+        response = requests.get(BASE + word).json()
+        print(response)
+        if response['found'] != True:
+            return False
         return True
